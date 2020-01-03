09b9623d1c4542b86ba5aa62ccf7d99c43f288d3
litanazareth <lita.anisha.nazareth@gmail.com>
2020-01-03 11:57:03 +0400

    Updates

---
+++ precache-manifest.ca8cf11e51643817e75992e1ecc1f8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f8c97903e24595b97642eba01c9979",
+    "url": "/ReactStarWarsApp/index.html"
+  },
+  {
+    "revision": "c3313fd97fe8315ea933",
+    "url": "/ReactStarWarsApp/static/css/main.908c5366.chunk.css"
+  },
+  {
+    "revision": "9f02b145357f0b068349",
+    "url": "/ReactStarWarsApp/static/js/2.3c70fe43.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ReactStarWarsApp/static/js/2.3c70fe43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3313fd97fe8315ea933",
+    "url": "/ReactStarWarsApp/static/js/main.14400108.chunk.js"
+  },
+  {
+    "revision": "74e340ae4923c822314c",
+    "url": "/ReactStarWarsApp/static/js/runtime-main.c8c1a8bf.js"
+  },
+  {
+    "revision": "0a12a758def6aa207e9ddbc3cade937e",
+    "url": "/ReactStarWarsApp/static/media/Star_Wars_Logo.0a12a758.svg"
+  }
+]);
